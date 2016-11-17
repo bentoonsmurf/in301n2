ec8b8eb0a955df2c8b810517ff1c0b3b108edb69
Utilisateur <you@example.com>
2016-11-17 14:02:30 +0100

    no

--- kore wa/Sudoku/lire_et_ecrire.c
+++ kore wa/Sudoku/lire_et_ecrire.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "sudoku.h"
+
+void ecrire(SUDOKU s){
+	FILE* f=fopen("sudoku.sudo","w");
+
+	int i=0;
+	int j=0;
+	for (i=0;i<9;i++){
+		for(j=0;j<9;j++){
+			fprintf(f,"%d ",S.la_case[i][j].val);
+		}
+		fprintf(f,"\n");
+	}
+	fclose(f);
+}
 
 
-FILE* f=fopen("sudoku.sudo","w");
-
-fprintf(f,"%d",S.la_case[x][y].val);
-
-fclose(f);
